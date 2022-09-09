a2b104ca8b119d898afac2d269da0a4992c7dc00
foyjur-192 <foyjur99@gmail.com>
2022-09-10 02:06:47 +0600

    admin update

--- index.js
+++ index.js
@@ -66,18 +66,33 @@ async function run(){
 // }
 
 
+//Get Admin
+app.get('/admin/:email', async(req, res) => {
+  const email = req.params.email;
+  const user = await userCollection.findOne({email: email});
+  const isAdmin = user.role === 'admin';
+  res.send({admin: isAdmin});
+})
 
 
 
   //Admin
-  app.put('/user/admin/:email', verifyJWT , async (req, res) => {
+  app.put('/user/admin/:email',verifyJWT, async (req, res) => {
     const email = req.params.email;
+   const requester = req.decoded.email;
+   const requesterAccount = await userCollection.findOne({email: requester})
+  if(requesterAccount.role === 'admin'){
     const filter = { email: email };
     const updateDoc = {
       $set: { role: 'admin' },
     };
     const result = await userCollection.updateOne(filter, updateDoc);
     res.send(result);
+  }
+  else{
+    res.status(403).send({message: 'forbidden access'});
+  }
+   
   })
 
 
